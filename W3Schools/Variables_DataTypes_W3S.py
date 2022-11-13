2b7d07c18582515b2619acdade4224eafe5dba9d
Rahat <105872616+Rahat-Kabir@users.noreply.github.com>
2022-11-13 07:14:32 +0600

    Add files via upload

---
+++ W3Schools/Variables_DataTypes_W3S.py
@@ -0,0 +1,81 @@
+
+"""
+print("Python Variables practice in W3S")
+
+#comment line testing
+
+#casting
+x=8
+y="THIS IS RAHAT"
+print(type(x))
+print(type(y))
+
+# This will create two variables
+a=6;
+A="Love"
+print(a)
+print(A)
+
+# Many Values to Multiple Variables
+x,y,z="JAVA","C++","PYTHON"
+print(x)
+print(y)
+print(z)
+
+x=y=z="FUN"
+print(x)
+print(y)
+print(z)
+
+# Unpack a list
+LanName=["java","c++","python"]
+x,y,z=LanName
+print(x)
+print(y)
+print(z)
+
+# Plus Operatior
+x="Who want"
+y="to be"
+z="KINGGGGG"
+print(x,y,z)
+print(x+y+z)
+
+a=6;
+A="DARK"
+print(a,A)
+print(a+A) #cant possible some int and str
+
+# Function
+x="Great"
+def myFunc():
+    x="Fantastic"
+    print("Python is " +x)
+myFunc()
+print("Python is "+x)
+
+# Global Function in Python
+x="Great"
+def myFunc():
+    global x
+    x="Fantastic"
+    print("Python is " +x)
+myFunc()
+print("Python is "+x)
+
+ ------------Python Data Type--------------
+x = 1j - complex
+x = ["apple", "banana", "cherry"] - list
+x = ("apple", "banana", "cherry") -	tuple
+x = range(6) -	range
+x = {"name" : "John", "age" : 36} -	dict
+x = {"apple", "banana", "cherry"} -	set
+x = frozenset({"apple", "banana", "cherry"}) -	frozenset
+x = True -	bool
+x = b"Hello" -	bytes
+x = bytearray(5) -	bytearray
+x = memoryview(bytes(5)) -	memoryview
+x = None -	NoneType
+"""
+print("Python Variables practice in W3S")
+
